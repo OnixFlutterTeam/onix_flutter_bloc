4abc1e19d4405f78429fee7186c7df9b8bc48ea5
alexandr.fedorenko <fedorenkoalex1992@gmail.com>
2024-10-08 14:24:39 +0300

    feat(core_models_update): updated core models

--- lib/src/bloc/base_cubit/base_cubit_state.dart
+++ lib/src/bloc/base_cubit/base_cubit_state.dart
@@ -5,7 +5,6 @@ import 'package:onix_flutter_bloc/src/bloc/base_cubit/base_cubit.dart';
 import 'package:onix_flutter_bloc/src/bloc/bloc_typedefs.dart';
 import 'package:onix_flutter_bloc/src/bloc/mixins/bloc_builders_mixin.dart';
 import 'package:onix_flutter_bloc/src/bloc/stream_listener.dart';
-import 'package:onix_flutter_bloc/src/domain/entity/progress_state/progress_state.dart';
 import 'package:onix_flutter_core_models/onix_flutter_core_models.dart';
 
 abstract class BaseCubitState<S, C extends BaseCubit<S, SR>, SR,
